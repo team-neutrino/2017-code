3c6455ac850890cb85f4051da24d3d18c63769ea
Logan Peters <LoganPeters@ahs887610pet34373.affiliate.iastate.edu>
2017-01-24 15:19:37 -0600

    First commit

---
+++ src/org/usfirst/frc/team3928/robot/Robot.java
@@ -0,0 +1,27 @@
+package org.usfirst.frc.team3928.robot;
+
+import edu.wpi.first.wpilibj.SampleRobot;
+
+
+public class Robot extends SampleRobot {
+
+	public Robot() {
+	}
+
+	@Override
+	public void robotInit() {
+	}
+
+	@Override
+	public void autonomous() {
+	}
+
+	@Override
+	public void operatorControl() {
+	
+	}
+
+	@Override
+	public void test() {
+	}
+}
