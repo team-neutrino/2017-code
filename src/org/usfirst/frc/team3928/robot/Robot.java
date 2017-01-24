2dca121534cd865098866ad837c4f4b72bc5a265
LoganPeters <LoganPeters@ahs887610pet34373.affiliate.iastate.edu>
2017-01-24 16:15:04 -0600

    Base drive code

--- src/org/usfirst/frc/team3928/robot/Robot.java
+++ src/org/usfirst/frc/team3928/robot/Robot.java
@@ -1,11 +1,20 @@
 package org.usfirst.frc.team3928.robot;
 
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.SampleRobot;
+import edu.wpi.first.wpilibj.Victor;
 
 
 public class Robot extends SampleRobot {
-
+	
+	Victor vic0;
+	Victor vic1;
+	Joystick rightJoy;
+	Joystick leftJoy;
+	
 	public Robot() {
+		vic0 = new Victor(0);
+		vic1 = new Victor(1);
 	}
 
 	@Override
@@ -18,7 +27,8 @@ public class Robot extends SampleRobot {
 
 	@Override
 	public void operatorControl() {
-	
+		vic0.set(Math.pow(leftJoy.getY(), 2));
+		vic1.set(Math.pow(rightJoy.getY(), 2));
 	}
 
 	@Override
