6afff3b8bca9e7d3c06c70e3055b7d65947fd181
TianxinXu <TianxinXu@ahs889373xu34747.student.iastate.edu>
2017-01-24 18:21:18 -0600

    Added constants

---
+++ src/org/usfirst/frc/team3928/robot/Constants.java
@@ -0,0 +1,20 @@
+package org.usfirst.frc.team3928.robot;
+
+public class Constants
+{
+	public static final boolean REAL_ROBOT = false;
+	
+	public static final int DRIVE_RIGHT_1_CHANNEL = 0;
+	public static final int DRIVE_RIGHT_2_CHANNEL = 1;
+	public static final int DRIVE_LEFT_1_CHANNEL = 2;
+	public static final int DRIVE_LEFT_2_CHANNEL = 3;
+	
+	public static final int DRIVE_RIGHT_1_POWER_CHANNEL = 0;
+	public static final int DRIVE_RIGHT_2_POWER_CHANNEL = 1;
+	public static final int DRIVE_LEFT_1_POWER_CHANNEL = 2;
+	public static final int DRIVE_LEFT_2_POWER_CHANNEL = 3;
+	
+	public static final int JOY_RIGHT_CHANNEL = 1;
+	public static final int JOY_LEFT_CHANNEL = 2;
+
+}
