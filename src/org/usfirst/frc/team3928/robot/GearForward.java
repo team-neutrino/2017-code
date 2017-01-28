4fcb3d8b4db371cc99578410353f1ea19e236678
Rucha Kelkar <680659kel@ames.k12.ia.us>
2017-01-28 12:34:01 -0600

    Added an auto mode and turnDegrees to drive.

--- src/org/usfirst/frc/team3928/robot/GearForward.java
+++ src/org/usfirst/frc/team3928/robot/GearForward.java
@@ -3,7 +3,21 @@ package org.usfirst.frc.team3928.robot;
 public class GearForward extends AutonModes{
 
 	@Override
-	public void run() {
+	public void run() 
+	{
+		Drive DriveInst = new Drive();
+		
+		//open flap TODO
+		DriveInst.driveDist(84.3, 0.5);
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+		DriveInst.driveDist(-42.15, -0.5);
+		//DriveInst.setLeft();
 	}
+	
 	
 }
