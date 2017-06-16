e2c93dbd537a7fbd955fb29f88e1be6133cb0e41
tianxin <tianxin@zhanyous-air.student.iastate.edu>
2017-06-16 17:12:36 -0500

    Changed shooter speed for demo.

--- src/org/usfirst/frc/team3928/robot/Constants.java
+++ src/org/usfirst/frc/team3928/robot/Constants.java
@@ -63,7 +63,7 @@ public class Constants
 	public static final double SHOOTER_P = 0.05; // TODO
 	public static final double SHOOTER_I = 0; // TODO
 	public static final double SHOOTER_D = 0; // TODO
-	public static final double SHOOTER_TARGET_SPEED = 54;
+	public static final double SHOOTER_TARGET_SPEED = 20; //competition = 54, demo = 
 
 	public static final int AGITATOR_CHANNEL = 0;
 	public static final int AGITATOR_POWER_CHANNEL = 9;
